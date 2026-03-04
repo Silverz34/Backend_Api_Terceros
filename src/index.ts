9fcb3b44676fcc26d1b1ccfc39c820cc8a39b42d
Silverz34 <pradogordillomichael@gmail.com>
2026-03-04 16:17:00 -0600

    comunicacion y puerto, corre en local por ahora

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from 'express'; 
+import cors from 'cors';
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT; 
+const API_URL = process.env.API_URL || '';
+
+app.use(cors({
+    origin: 'http://localhost:4000', 
+    methods: ['GET'] 
+}));
+
+
+app.get('/api', async (req: Request, res: Response) => {
+   try {
+        const response = await axios.get(API_URL);
+        const personajes = response.data.data;
+        res.status(200).json({
+            success: true,
+            data: personajes
+        });
+
+    } catch (error) {
+        console.error("Error al consultar la API de Disney:", error);
+        res.status(500).json({
+            success: false,
+            message: "Error de comunicación con el servidor externo."
+        });
+    }
+});
+
+app.listen(PORT, () =>{
+    console.log(`Servidor BFF corre exitosamente http://localhost:${PORT}`)
+})
